636f50ccffde339dc74c6fb798237679d73d8fa8
Bifabig <biftubig@gmail.com>
2023-09-08 12:42:57 +0300

    add models

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_many :entities, foreign_key: 'author_id'
+  has_many :groups
+
+  validates :name, presence: true
+end
