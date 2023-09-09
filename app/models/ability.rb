d86a1cf5329c1f09ad9e94b442dcd3c64f3c2a23
Bifabig <biftubig@gmail.com>
2023-09-09 11:18:25 +0300

    modify models and roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,13 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    can :read, Entity, public: true
+    can :read, Group, public: true
+
+    return unless user.present?
+
+    can :manage, Entity, user:
+    can :manage, Group, user:
+  end
 end
