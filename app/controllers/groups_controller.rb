81a9f161fbb10d37e9e42ba2ab0491381ed9b3c5
Bifabig <biftubig@gmail.com>
2023-09-12 21:11:05 +0300

    add actions to controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,47 @@
 class GroupsController < ApplicationController
-  before_action :set_groups, only: %i[show edit update destroy]
-
   def index
-    @user = current_user
-    if @user.nil?
-      redirect_to user_session_path, flash: { alert: 'You must be signed in to continue.' }
+    @groups = current_user.groups
+  end
+
+  def show
+    @group = Group.find_by(id: params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.create(group_params)
+    group.user = current_user
+
+    if group.save
+      redirect_to group_path(group)
+      flash[:notice] = 'Group created successfully'
     else
-      @groups = @user.groups
+      redirect_to new_group_path
+      flash[:alert] = group.errors.full_messages.join(', ')
     end
+  end
+
+  def edit
+    @group = Group.find_by(id: params[:id])
+  end
+
+  def update
+    group = Group.find_by(id: params[:id])
+    group.update(group_params)
+    redirect_to group_path(group)
+  end
+
+  def destroy
+    Group.destroy(params[:id])
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
