b3481961a01619254c847557b4b98ead9e688cb8
Bifabig <biftubig@gmail.com>
2023-09-13 11:38:29 +0300

    modify migration files

--- db/migrate/20230908082254_create_entities.rb
+++ db/migrate/20230908082254_create_entities.rb
@@ -2,7 +2,6 @@ class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
       t.references :author, foreign_key: { to_table: :users }
-      t.references :group, foreign_key: true
       t.string :name
       t.integer :amount
 
---
+++ db/migrate/20230912087532_create_entities_group_to_joins.rb
@@ -0,0 +1,9 @@
+class CreateEntitiesGroupToJoins < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :entities, :groups do |t|
+      t.index :entity_id
+      t.index :group_id
+    end
+  end
+end
+
